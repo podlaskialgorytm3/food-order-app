bbffb022a9ba865e4177fcd29ab429657d2cf4e4
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-20 21:12:59 +0100

    Add Cart component to Header and update button styles

--- src/features/header-feature/Header.jsx
+++ src/features/header-feature/Header.jsx
@@ -1,9 +1,11 @@
 import { Title } from '../header-feature/components/Title'
+import { Cart } from '../header-feature/components/Cart'
 
 export const Header = () => {
     return (
         <header id="main-header">
             <Title>ReactFood</Title>
+            <Cart>Cart</Cart>
         </header>
     );
     }
---
+++ src/features/header-feature/components/Cart.jsx
@@ -0,0 +1,7 @@
+export const Cart = ({children}) => {
+    return(
+        <button>
+            {children}
+        </button>
+    )
+}
