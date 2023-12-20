532bc2a69ec9ca2f92e085874c334c1f341366f8
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-20 21:59:46 +0100

    Add MealItem component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./features/header-feature/Header";
 import { MealsContainer } from "./features/meals-feature/MealsContainer";
+import { MealItem } from "./features/meals-feature/components/MealItem";
 
 import { useFetch } from "./hooks/useFetch";
 
@@ -12,7 +13,18 @@ function App() {
     <>
       <Header />
       <MealsContainer>
-        
+            {isLoading && <p>Loading...</p>}
+            {error && <p>{error.message}</p>}
+            {!isLoading && (
+              meals.map((meal) => 
+              ( <MealItem 
+              key={meal.id} 
+              name={meal.name} 
+              price={meal.price} 
+              description={meal.description}
+              imgage={meal.image}
+              />)
+              ))}
       </MealsContainer>
     </>
   );
--- src/features/meals-feature/components/MealItem.jsx
+++ src/features/meals-feature/components/MealItem.jsx
@@ -1,17 +1,17 @@
-export const MealItem = () => {
+export const MealItem = ({name,price,description,image}) => {
 
     return(
         <div className="meal-item">
-            <img />
-            <h3></h3>
             <article>
+                <img src={image} />
+                <h3>{name}</h3>
                 <p className="meal-item-price">
-
+                    {price}
                 </p>
                 <p className="meal-item-description">
-
+                    {description}
                 </p>
-                <div className="meal-item-description">
+                <div className="meal-item-actions">
                     <button>Add to Cart</button>
                 </div>
             </article>
